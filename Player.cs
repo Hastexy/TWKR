1d9780b8a8a3015dd2c7b554a29340da0cc937b2
Arnþór Árni <70693270+Arnpor@users.noreply.github.com>
2024-06-18 18:59:18 +0000

    L movement

--- Player.cs
+++ Player.cs
@@ -2,16 +2,15 @@ using Godot;
 using Godot.Collections;
 using System;
 using System.Diagnostics;
+using System.Linq;
 
 public partial class Player : Node2D
 {
-	[Export] public TileMap[] indicators;
-	public bool first = true;
-	public int first_move = 2;
-	public int second_move = 1;
+	public int moves = 3;
+	public int[] legalmoves;
 	public override void _Ready()
 	{
-		indicators[0].Visible = true;
+		legalmoves = [2, 2, 2, 2];
 		Position = new Vector2(9,9);
 	}
 
@@ -20,50 +19,76 @@ public partial class Player : Node2D
 		Move();
 	}
 
+	public void printbug(){
+		string text = "[";
+		foreach(var item in legalmoves)
+		{
+			text += item;
+		}
+		Debug.Print(text+"] moves: "+moves);
+	}
+
 	public void Move()
 	{
 		Vector2 pos = Position;
-		int multiplier = first ? first_move : second_move;
 		
-		if (Input.IsActionJustPressed("Move Right"))
+		if (Input.IsActionJustPressed("Move Right") && legalmoves[0] > 0)
     	{
-        	pos.X += 18 * multiplier;
-			SwitchIndicators(true);
+			moves--;
+			legalmoves[0]--;
+			legalmoves[1] = 0;
+			if (legalmoves[2] == 1 || legalmoves[3] == 1){
+				legalmoves[2] = 0;
+				legalmoves[3] = 0;
+			}
+			printbug();
+        	pos.X += 18;
     	}
 
-		if (Input.IsActionJustPressed("Move Left"))
+		if (Input.IsActionJustPressed("Move Left") && legalmoves[1] > 0)
 		{
-			pos.X -= 18 * multiplier;
-			SwitchIndicators(true);
+			moves--;
+			legalmoves[1]--;
+			legalmoves[0] = 0;
+			if (legalmoves[2] == 1 || legalmoves[3] == 1){
+				legalmoves[2] = 0;
+				legalmoves[3] = 0;
+			}
+			printbug();
+			pos.X -= 18;
 		}
 
-		if (Input.IsActionJustPressed("Move Down"))
+		if (Input.IsActionJustPressed("Move Down") && legalmoves[2] > 0)
 		{
-			pos.Y += 18 * multiplier;
-			SwitchIndicators(false);
+			moves--;
+			legalmoves[2]--;
+			legalmoves[3] = 0;
+			if (legalmoves[1] == 1 || legalmoves[0] == 1){
+				legalmoves[1] = 0;
+				legalmoves[0] = 0;
+			}
+			printbug();
+			pos.Y += 18;
 		}
 
-		if (Input.IsActionJustPressed("Move Up"))
+		if (Input.IsActionJustPressed("Move Up") && legalmoves[3] > 0)
 		{
-			pos.Y -= 18  * multiplier;
-			SwitchIndicators(false);
+			moves--;
+			legalmoves[3]--;
+			legalmoves[2] = 0;
+			if (legalmoves[1] == 1 || legalmoves[0] == 1){
+				legalmoves[1] = 0;
+				legalmoves[0] = 0;
+			}
+			printbug();
+			pos.Y -= 18;
 		}
 
 		Position = pos;
+		if (moves <= 0){
+			moves = 3;
+			legalmoves = [2, 2, 2, 2];
+		}
 	}
 
-	public void SwitchIndicators(bool horizontal){
-		if (first){
-			indicators[0].Visible = false;
-			indicators[1].Visible = !horizontal;
-			indicators[2].Visible = horizontal;
-		}
-		else
-		{
-			indicators[1].Visible = false;
-			indicators[2].Visible = false;
-			indicators[0].Visible = true;
-		}
-		first = !first;
-	}
 }
