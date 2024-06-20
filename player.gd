af9d9b56d00511e57d5ff7b13d328b6805bafcfa
Arnþór Árni <70693270+Arnpor@users.noreply.github.com>
2024-06-20 14:02:20 +0000

    All endpoints indicated

--- player.gd
+++ player.gd
@@ -3,63 +3,65 @@ extends Node
 @export var floor_tilemap: TileMap
 @export var player: Node2D
 @export var selected_tile: Sprite2D
+@export var endpoints: Node2D
 var moves = 3
 var legal_moves = [2,2,2,2]
 var walkable_tiles
 var pos = Vector2i(0,0)
 var move_selected = false
 var move_index = 0
-var endpoints
+var endpoints_list
 
 func _ready():
 	player.position = Vector2i(9,9)
 	walkable_tiles = floor_tilemap.get_used_cells(0)
 	IndicateMoves()
+	DrawEndpoints()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	DrawIndicators()
+	DrawSelect()
 	SelectMove()
 	if move_selected:
 		IndicateMoves()
+		DrawEndpoints()
 	
-func DrawIndicators():
-	selected_tile.position = player.position + Vector2(endpoints[move_index].x, endpoints[move_index].y)*18
+func DrawSelect():
+	selected_tile.position = player.position + Vector2(endpoints_list[move_index].x, endpoints_list[move_index].y)*18
+	
+func DrawEndpoints():
+	for child in endpoints.get_children():
+		endpoints.remove_child(child)
+	print(endpoints.get_children())
+	for point in endpoints_list:
+		var tile = selected_tile.duplicate()
+		tile.position = player.position + Vector2(point.x, point.y)*18
+		tile.modulate.a8 = 75
+		endpoints.add_child(tile)
+		
 	
 func IndicateMoves():
-	endpoints = GetEndpoints()
-	if move_index >= endpoints.size():
+	endpoints_list = GetEndpoints()
+	if move_index >= endpoints_list.size():
 		move_index = 0
 	move_selected = false
 	
 func SelectMove():
-	var cur_move = endpoints[move_index]
+	var cur_move = endpoints_list[move_index]
 	if Input.is_action_just_pressed("Move Right"):
 		move_index += 1
-		if move_index == endpoints.size():
+		if move_index == endpoints_list.size():
 			move_index = 0
-		cur_move = endpoints[move_index]
+		cur_move = endpoints_list[move_index]
 	elif Input.is_action_just_pressed("Move Left"):
 		move_index -= 1
 		if move_index == -1:
-			move_index = endpoints.size()
-		cur_move = endpoints[move_index]
+			move_index = endpoints_list.size() - 1
+		cur_move = endpoints_list[move_index]
 	elif Input.is_action_just_pressed("Select"):
 		player.position += Vector2(cur_move.x, cur_move.y)*18
 		pos += cur_move
 		move_selected = true
-		
-func EditMoves(curside, opposite, other, oppositeother, transform):
-	moves -= 1
-	legal_moves[curside] -= 1
-	legal_moves[opposite] = 0
-	if (legal_moves[other] == 1 or legal_moves[oppositeother] == 1):
-		legal_moves[other] = 0
-		legal_moves[oppositeother] = 0
-	var x = player.position.x + transform.x
-	var y = player.position.y + transform.y
-	player.position = Vector2i(x,y)
-	pos += transform/18
 	
 func GetEndpoints():
 	var endpoint_base = [Vector2i(1,-2), Vector2i(2,-1), Vector2i(2,1), Vector2i(1,2), Vector2i(-1,2), Vector2i(-2,1), Vector2i(-2,-1), Vector2i(-1,-2)]
