a91583a5d613f1f3f7a9d46f533a1cecd7b21c3a
Arnþór Árni <70693270+Arnpor@users.noreply.github.com>
2024-06-22 17:25:15 +0000

    levelgeneration

--- Scripts/LevelGenerator.gd
+++ Scripts/LevelGenerator.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
-@export var generation_size : Vector2 = Vector2(5, 3)
-
 @export var room_list = ["res://Scenes/Rooms/Room1.tscn", "res://Scenes/Rooms/Room2.tscn"]
 var room_size = Vector2i(324, 180)
+var room_count = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var tilemap_array_x = []
-	for x in generation_size.x:
-		var tilemap_array_y = []
-		for y in generation_size.y:
-			var random_room = load(room_list.pick_random())
-			var r = random_room.instantiate()
-			
-			r.position.x = x * room_size.x
-			r.position.y = y * room_size.y
-			add_child(r)
-		tilemap_array_x.append(tilemap_array_y)
+	var current_room = Vector2i(0,0)
+	var tilemap_array = []
+	for i in room_count:
+		tilemap_array.append([])
+		for j in room_count:
+			tilemap_array[i].append(false)
+	var random_room = load("res://Scenes/Rooms/Room1.tscn")
+	while room_count > 0:
+		var r = random_room.instantiate()
+		r.position.x = current_room.x * room_size.x
+		r.position.y = current_room.y * room_size.y
+		add_child(r)
+		tilemap_array[current_room.x][current_room.y] = true
+		room_count -= 1
+		current_room = Vector2i(0,0)
+		while tilemap_array[current_room.x][current_room.y] == true:
+			current_room += [Vector2i(0,1),Vector2i(1,0),Vector2i(0,-1),Vector2i(-1,0)].pick_random()
+		random_room = load(room_list.pick_random())
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
