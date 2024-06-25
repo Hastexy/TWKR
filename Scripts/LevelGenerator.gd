16d2d5ae47f3accff3d1af134cde913cb5a3320b
Arnþór Árni <70693270+Arnpor@users.noreply.github.com>
2024-06-25 22:30:32 +0000

    Pawns spawning everywhere

--- Scripts/LevelGenerator.gd
+++ Scripts/LevelGenerator.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 @export var room_list = ["res://Scenes/Rooms/Room1.tscn", "res://Scenes/Rooms/Room2.tscn"]
+@export var enemy_list = ["res://Scenes/Pawn.tscn"]
 var room_size = Vector2i(324, 180)
 var room_count = 15
+var enemy_count = [0, 1, 1, 1, 2, 2, 3]
+
+@onready var rooms = $Rooms
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,15 +21,35 @@ func _ready():
 		var r = random_room.instantiate()
 		r.position.x = current_room.x * room_size.x
 		r.position.y = current_room.y * room_size.y
-		add_child(r)
+		rooms.add_child(r)
 		tilemap_array[current_room.x][current_room.y] = true
 		room_count -= 1
 		current_room = Vector2i(0,0)
 		while tilemap_array[current_room.x][current_room.y] == true:
 			current_room += [Vector2i(0,1),Vector2i(1,0),Vector2i(0,-1),Vector2i(-1,0)].pick_random()
 		random_room = load(room_list.pick_random())
+	SpawnEnemies()
+	pass
 		
-
+func SpawnEnemies():
+	var room_nodes = rooms.get_children()
+	for room in room_nodes:
+		var enemy_node = Node2D.new()
+		enemy_node.name = "Enemies"
+		room.add_child(enemy_node)
+		
+		var spawns = enemy_count.pick_random()
+		var spawnable_tiles = room.get_child(0).get_used_cells(0)
+		for i in spawns:
+			var random_enemy = load(enemy_list.pick_random()).instantiate()
+			enemy_node.add_child(random_enemy)
+			var tile = spawnable_tiles.pick_random()
+			random_enemy.pos = tile
+			random_enemy.position = random_enemy.pos*18
+	pass
+		
+		
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,34 +2,57 @@ extends Node
 
 var move = 1
 var walkable_tiles
-var pos = Vector2i(1,1)
+var pos
 var time = 0
-var enemy_collider
 var attacking = false
+
+@onready var enemy_collider = $EnemyCollider
+@onready var movement_endpoints = $"Movement Colliders"
+@onready var killing_endpoints = $"Killing Colliders"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
-	self.position = pos * 18
-	enemy_collider = $EnemyCollider
-	
+	pass
 	
 func _process(delta):
 	time += delta
 	if time >= 1.0:
-		attacking = true
 		var next = getNextMove()
 		MoveEnemy(next)
 		time = 0
 		
 func getNextMove():
-	move *= -1
-	return Vector2i(0, move)
+	#Check if you can kill player
+	var endpoints = killing_endpoints.get_children()
+	for endpoint : Area2D in endpoints:
+		var overlap_areas = endpoint.get_overlapping_areas() != []
+		var overlap_player = false
+		if overlap_areas:
+			overlap_player = endpoint.get_overlapping_areas()[0].name == "PlayerArea"
+		if overlap_player:
+			attacking = true
+			return endpoint.get_meta("Direction")
+			
+	#If enemy can't kill player, just move randomly
+	var valid_endpoints = []
+	endpoints = movement_endpoints.get_children()
+	for endpoint : Area2D in endpoints:
+		var in_bounds = endpoint.get_overlapping_bodies() != []
+		var overlap_areas = endpoint.get_overlapping_areas() != []
+		var overlap_player
+		if overlap_areas:
+			print(endpoint.get_overlapping_areas()[0].name)
+			overlap_player = endpoint.get_overlapping_areas()[0].name == "PlayerArea"
+		if in_bounds and not overlap_player:
+			valid_endpoints.append(endpoint)
+	var endpoint = valid_endpoints.pick_random()
+	return endpoint.get_meta("Direction")
 	
 	
 func MoveEnemy(movement : Vector2i):
-	pos += movement
+	pos += movement 
 	var enemy_movement_tween = create_tween()
-	enemy_movement_tween.tween_property(self, "global_position", Vector2(pos * 18), 0.1).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
+	enemy_movement_tween.tween_property(self, "position", Vector2(pos * 18), 0.1).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
 	await enemy_movement_tween.finished
 	await get_tree().create_timer(0.1).timeout
 	attacking = !enemy_movement_tween.finished
@@ -43,3 +66,4 @@ func _on_enemy_collider_area_entered(area : Area2D):
 		player.dead = true
 	else:
 		self.get_parent().remove_child(self)
+		self.queue_free()
